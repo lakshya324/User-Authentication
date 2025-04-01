0a2717372480d49a6aa6999fbefcab52997ef927
Lakshya Sharma <lakshya.off31@gmail.com>
2025-04-01 22:53:09 +0530

    Add email and phone existence validation functions

--- src/utils/user.validate.ts
+++ src/utils/user.validate.ts
@@ -20,3 +20,23 @@ export async function isEmailExist(email: string): Promise<boolean> {
     createError("Error fetching data from the database", 500);
   }
 }
+
+/**
+ * This function is used to check if the phone number is already taken.
+ * @param phone - phone number to be checked
+ * @returns boolean
+ * @throws Error if any error occurs while fetching data from the database
+ * @example
+ * isPhoneExist("1234567890")
+ * // returns true
+ * @example
+ * isPhoneExist("0987654321")
+ * // returns false
+ */
+export async function isPhoneExist(phone: string): Promise<boolean> {
+  try {
+    return (await UserDB.findOne({ phone })) ? true : false;
+  } catch (err) {
+    createError("Error fetching data from the database", 500);
+  }
+}
